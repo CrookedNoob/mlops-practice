872850417a1618b1adbcbca3576c6ac015a71a8f
crookednoob <soumya_dip@live.in>
2023-04-11 05:23:52 +0000

    added first model to mlflow tracking

---
+++ 2_experiment_tracking/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from preprocess_model import ModelTracking
+
+mt = ModelTracking()
+
+df_train = mt.read_dataframe("./dataset/green_tripdata_2022-10.parquet")
+df_val = mt.read_dataframe("./dataset/green_tripdata_2022-11.parquet")
+
+X_train, X_val, y_train, y_val = mt.feature_target_set(df_train, df_val)
+
+
+mt.lasso_tracker(ds_name="Soumyadip Majumder",
+                train_path="./dataset/green_tripdata_2022-10.parquet",
+                val_path="./dataset/green_tripdata_2022-11.parquet",
+                alpha=0.1,
+                model_name="lasso_nyc_pred_1.bin",
+                X_train=X_train, y_train=y_train,
+                X_val=X_val, y_val=y_val)
+
